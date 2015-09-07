ccc110714149ab63c4326a015ee764f696558df3
Ben Halverson <benhalverson33@gmail.com>
2015-09-07 16:44:53 -0700

    setting up Aurelia

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 export class App {
 
   constructor() {
+    this.message = "";
+  }
+  activate() {
     this.message = "Hello from Aurelia!";
   }
-
+  changeMessage() {
+    this.message = "Goodbye";
+  }
 }
--- greeter.js
+++
@@ -1,7 +0,0 @@
-/*jslint node: true */
-"use strict";
-var message = "hello";
-
-export function sayHello() {
-  console.log(message);
-}
